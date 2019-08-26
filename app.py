494b84d35e768371ec54673d01449f5499d15a6e
Sachin Kundu <sakundu@microsoft.com>
2019-08-26 16:57:15 +0300

    restoring to original state

--- app.py
+++ app.py
@@ -5,13 +5,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Flask Dockerized'
 
-@app.route('/demo')
-def hello_demo():
-    return 'Hello from Demo'
-
-@app.route('/demo2')
-def hello_again():
-    return 'Hello Again'
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')

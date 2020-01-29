ffddc82dde5262753cd66ca81283bc291540d38d
Sachin Kundu <sachinkundu@gmail.com>
2020-01-29 11:23:11 +0200

    add hostname option

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM ubuntu:latest
-RUN apt-get update -y
-RUN apt-get install -y python-pip python-dev build-essential
+RUN apt-get update \
+  && apt-get install -y python3-pip python3-dev \
+  && cd /usr/local/bin \
+  && ln -s /usr/bin/python3 python \
+  && pip3 install --upgrade pip
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import subprocess
 from flask import Flask
+
 app = Flask(__name__)
+
+def get_hostname():
+    hostname_call = subprocess.run(["hostname", "-i"], stdout=subprocess.DEVNULL)
+    return hostname_call.stdout
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return f'Flask Dockerized from {get_hostname()}'
 
 @app.route('/demo')
 def demo():
-    return 'hello from demo'
+    return f'hello from demo from {get_hostname()}'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
